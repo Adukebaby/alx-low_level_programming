4bc47fe35d22361aee761510a69b36e5b91cdd63
Adukebaby <mzadukecares@gmail.com>
2023-08-15 15:52:07 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main  -Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar ('\n');
+
+	return (0);
+}
