de57160d753952ab7b1a9ea634359ce8509552db
Adukebaby <mzadukecares@gmail.com>
2023-08-16 14:48:35 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,22 @@
 
 /**
  * print_alphabet_x10 - Make alphabet x10 times
- *
+ * Description - we cant use standard library to implement our functions
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
-	char c;
 	int i = 0;
-	{
-		for (c = 'a'; c <= 'z'; c++)
+
+		for (i = 0; i < 10; i++)
 		{
-			_putchar (c);
+			char c;
+
+			for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
+			_putchar('\n');
 		}
-
-		_putchar ('\n')
-
-		i++;
 	}
-}
-
