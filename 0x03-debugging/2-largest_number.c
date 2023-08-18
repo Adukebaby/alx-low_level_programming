7de719cefcb0d6bf0956d7f2922c3fcf9f90d1bd
Adukebaby <mzadukecares@gmail.com>
2023-08-18 03:09:44 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,16 +14,19 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-	largest = a;
+		largest = a;
 	}
 	else if (b > c)
 	{
-	largest = b;
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
-
 	return (largest);
-
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,6 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_date(int month, int day, int year);
-int convert_day(int month, int day);
+largest_number - returns the largest of 3 numbers
 
 #endif /* MAIN_H */
