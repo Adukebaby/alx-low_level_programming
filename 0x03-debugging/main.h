09e8c98984879307b21469c648c8cdf55e407448
Adukebaby <mzadukecares@gmail.com>
2023-08-18 03:18:04 -0700

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <main.h>
+
+#include <stdio.h>
 
 void positive_or_negative(int i);
+
 int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
 void print_remaining_date(int month, int day, int year);
+
 largest_number - returns the largest of 3 numbers
 
 #endif /* MAIN_H */
